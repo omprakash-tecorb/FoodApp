53f3e8d3fb9f1c7340f68404ca3b3c108ee0efa0
Om Prakash <omprakash.2022@tecorb.co>
2022-11-08 12:12:28 +0530

    Initial Commit

---
+++ PizzaOnTheGo/PizzaOnTheGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaOnTheGoApp.swift
+//  PizzaOnTheGo
+//
+//  Created by Apple on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaOnTheGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
